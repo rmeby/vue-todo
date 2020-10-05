d3b931bdaa195598e6dbcd2d4e2cbd8e5968b1b4
Ross Eby <44152538+rmeby@users.noreply.github.com>
2020-10-04 21:50:30 -0400

    Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="view">
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@500&display=swap" rel="stylesheet">
+    <div id="title">
+      <h3>Vue ToDo</h3>
+    </div>
+    <div id="tasks">
+      <Task />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from './components/Task.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Task
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: 'Mukta', sans-serif;
 }
 </style>
